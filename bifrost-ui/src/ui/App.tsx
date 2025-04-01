67f01bb580d15091c9acb94712515b9488f94a0c
Apiksha <panditaapiksha@gmail.com>
2025-04-01 17:08:23 +0530

    Added Resource Manager

--- bifrost-ui/src/ui/App.tsx
+++ bifrost-ui/src/ui/App.tsx
@@ -6,11 +6,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="App">
       <div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> 
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -18,13 +18,13 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit uuyh <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
