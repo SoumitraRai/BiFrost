f73739a5f23f8635cde2d85b899cee10477d2e17
Apiksha <118296166+Apiksha@users.noreply.github.com>
2025-04-09 16:39:20 +0530

    update bifrost-ui

---
+++ bifrost-ui/src/ui/App.tsx
@@ -0,0 +1,11 @@
+// src/ui/App.tsx
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>Hello from Electron + React!</h1>
+      <p>Time to build your own app 🎉</p>
+    </div>
+  );
+}
+
+export default App;
